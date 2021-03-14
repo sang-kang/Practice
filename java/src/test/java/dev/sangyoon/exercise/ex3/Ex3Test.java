cc16ceabbc217180651296d06b78ddbb2c453074
Dev <whitepencil.london@gmail.com>
2021-03-14 12:28:00 +0000

    ex3 bug correction & add test case

--- java/src/test/java/dev/sangyoon/exercise/ex3/Ex3Test.java
+++ java/src/test/java/dev/sangyoon/exercise/ex3/Ex3Test.java
@@ -11,13 +11,20 @@ public class Ex3Test {
     private PassGenerator generator;
 
     @Test
-    public void test() {
-        assertListEquals(Arrays.asList("ac, at, ct"), generator.generateAll(Arrays.asList('c', 'a', 't'), 2));
+    public void test1() {
+        assertListEquals(Arrays.asList("ac", "at", "ct"), generator.generateAll(Arrays.asList('c', 'a', 't'), 2));
+    }
+
+    @Test
+    public void test2() {
+        assertListEquals(Arrays.asList("ac", "ai", "as", "at", "aw", "ci", "cs", "ct", "cw", "is", "it", "iw", "st", "sw", "tw"), 
+        generator.generateAll(Arrays.asList('c', 'a', 't', 'i', 's', 'w'), 3));
     }
 
     private void assertListEquals(List<String> expected, List<String> actual) {
         for(int i = 0; i < expected.size(); i++){
-            assertTrue(expected.get(i).compareTo(actual.get(i)) == 0, String.format("expected %s, actual %s", expected.get(i), actual.get(i));
+            assertTrue(expected.get(i).compareTo(actual.get(i)) == 0, 
+            String.format("expected %s, actual %s", expected.get(i), actual.get(i)));
         }
     }
 }
