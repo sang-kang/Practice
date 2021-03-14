6a251e2eace1b89392417a824ecc52751f529a66
Dev <whitepencil.london@gmail.com>
2021-03-14 14:12:58 +0000

    ex3 fixed test2

--- java/src/test/java/dev/sangyoon/exercise/ex3/Ex3Test.java
+++ java/src/test/java/dev/sangyoon/exercise/ex3/Ex3Test.java
@@ -17,11 +17,13 @@ public class Ex3Test {
 
     @Test
     public void test2() {
-        assertListEquals(Arrays.asList("ac", "ai", "as", "at", "aw", "ci", "cs", "ct", "cw", "is", "it", "iw", "st", "sw", "tw"), 
-        generator.generateAll(Arrays.asList('c', 'a', 't', 'i', 's', 'w'), 3));
+        assertListEquals(
+            Arrays.asList("aci", "acs", "act", "ais", "ait", "ast", "cis", "cit", "cst", "ist"), 
+            generator.generateAll(Arrays.asList('c', 'a', 't', 'i', 's'), 3));
     }
 
     private void assertListEquals(List<String> expected, List<String> actual) {
+        System.out.println(actual);
         for(int i = 0; i < expected.size(); i++){
             assertTrue(expected.get(i).compareTo(actual.get(i)) == 0, 
             String.format("expected %s, actual %s", expected.get(i), actual.get(i)));
